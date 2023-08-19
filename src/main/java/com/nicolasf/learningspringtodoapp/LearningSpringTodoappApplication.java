3e021c14dc1e80e994b096a46ca71ec6fe2be8de
Nicolas <nicolas@nicolasf.uy>
2023-08-19 10:00:56 -0300

    Start section 7

---
+++ src/main/java/com/nicolasf/learningspringtodoapp/LearningSpringTodoappApplication.java
@@ -0,0 +1,13 @@
+package com.nicolasf.learningspringtodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringTodoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringTodoappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicolasf/learningspringtodoapp/LearningSpringTodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolasf.learningspringtodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringTodoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
